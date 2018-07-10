3761692c1c7ea0812721915d3afab4dbb3d0fb58
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-07-10 22:57:21 +0900

    first commit

---
+++ resource-api/src/main/java/com/example/resourceapi/ResourceApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.resourceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceApiApplication.class, args);
+	}
+}
