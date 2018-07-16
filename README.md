7230ea7c7e1528d81db487886be211ed8e8ef764
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-07-17 00:14:48 +0900

    Update for OAuth access conrtoll

--- README.md
+++ README.md
@@ -2,17 +2,38 @@
 
 ### Resource Server
 
-#### REST API
+#### REST API 
 - Insert
 ```
-$ curl -X POST -u user:password -H 'Content-Type: application/json' -d '{"task":"Task Name"}' http://localhost:8080/v1/task
+$ curl -v -X POST -u user:password -H 'Content-Type: application/json' -d '{"task":"Task Name"}' http://localhost:8080/v1/task
 ```
 - Find all
 ```
-$ curl -X GET -u user:password http://localhost:8080/v1/tasks
+$ curl -v -X GET -u user:password http://localhost:8080/v1/timelines
 ```
 - Find by user
 ```
-$ curl -X GET -u user:password http://localhost:8080/v1/tasks
+$ curl -v -X GET -u user:password http://localhost:8080/v1/tasks
 ```
 
+#### REST API (with Access Token)
+- Insert
+```
+curl -v -X POST -H 'Authorization: Bearer e257d9c6-d21f-44c5-a517-dce7e31fb6b5' -H 'Content-Type: application/json' -d '{"task":"Task Name"}'  http://localhost:18082/v1/task
+```
+
+- Find all
+```
+curl -v -X GET -H 'Authorization: Bearer e257d9c6-d21f-44c5-a517-dce7e31fb6b5' http://localhost:18082/v1/timelines
+```
+
+
+
+### Authorization Server
+
+#### OAuth Endpoint
+
+- Token Endpoint
+```
+curl -X POST -u demo:demo -d grant_type=password -d username=user -d password=password http://localhost:18081/authz/oauth/token
+```
--- resource-server/src/main/java/com/example/taskapi/TaskApiApplication.java
+++ resource-server/src/main/java/com/example/taskapi/TaskApiApplication.java
@@ -2,11 +2,31 @@ package com.example.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
 
 @SpringBootApplication
 public class TaskApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(TaskApiApplication.class, args);
+  }
+
+  @Configuration
+  @EnableResourceServer
+  static class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+          .antMatchers(HttpMethod.GET, "/v1/**").access("#oauth2.hasScope('read')")
+          .antMatchers(HttpMethod.POST, "/v1/**").access("#oauth2.hasScope('write')")
+          .antMatchers(HttpMethod.PUT, "/v1/**").access("#oauth2.hasScope('write')")
+          .antMatchers(HttpMethod.DELETE, "/v1/**").access("#oauth2.hasScope('write')")
+          .antMatchers(HttpMethod.PATCH, "/v1/**").access("#oauth2.hasScope('write')");
+    }
   }
 }
