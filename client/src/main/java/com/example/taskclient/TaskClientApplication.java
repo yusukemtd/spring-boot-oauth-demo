90bac89c76d8cb3a39f0fbcfe3e12810c05cf9c4
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-07-28 23:46:18 +0900

    Add OAuth Client

---
+++ client/src/main/java/com/example/taskclient/TaskClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.taskclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskClientApplication.class, args);
+	}
+}
