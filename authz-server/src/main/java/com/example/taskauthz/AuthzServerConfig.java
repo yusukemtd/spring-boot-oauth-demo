beec4da6d659ae85f0a6f3465097c093f0ce5b83
Yusuke Matsuda <yusuke.madd@gmail.com>
2018-07-31 10:47:51 +0900

    Add JWT to Authz config

--- authz-server/src/main/java/com/example/taskauthz/AuthzServerConfig.java
+++ authz-server/src/main/java/com/example/taskauthz/AuthzServerConfig.java
@@ -3,6 +3,7 @@ package com.example.taskauthz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -11,14 +12,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthzServerConfig extends AuthorizationServerConfigurerAdapter {
 
-  @Autowired
-  private TokenStore tokenStore;
+//  @Autowired
+//  private TokenStore tokenStore;
 
   @Autowired
   private UserApprovalHandler userApprovalHandler;
@@ -46,12 +50,36 @@ public class AuthzServerConfig extends AuthorizationServerConfigurerAdapter {
 
   @Override
   public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-    endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
-        .authenticationManager(authenticationManager);
+    endpoints.tokenStore(tokenStore())
+      .accessTokenConverter(accessTokenConverter())
+      .userApprovalHandler(userApprovalHandler)
+      .authenticationManager(authenticationManager);
   }
   
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JwtTokenStore(accessTokenConverter());
+  }
+
+  @Bean
+  public JwtAccessTokenConverter accessTokenConverter() {
+    JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+    // MAC key
+    converter.setSigningKey("123");
+    return converter;
+  }
+
+  @Bean
+  @Primary
+  public DefaultTokenServices tokenServices() {
+    DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+    defaultTokenServices.setTokenStore(tokenStore());
+    defaultTokenServices.setSupportRefreshToken(true);
+    return defaultTokenServices;
   }
 }
